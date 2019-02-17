b6a42be228f8b83bedca39d8defbd785b8831d4f
Giacomo <tehpepsy@gmail.com>
2019-02-17 23:26:08 +0100

    ok

--- CifrarioCesare.py
+++ CifrarioCesare.py
@@ -1,4 +1,4 @@
-alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
 
 testo_da_cryptare = raw_input("Digita un testo: ").upper()
 
@@ -7,9 +7,9 @@ lunghezza_testo = len(testo_da_cryptare)
 testo_cryptato = ""
 
 for i in range(lunghezza_testo):
-    location_char = alfabeto.find(testo_da_cryptare[i])
+    location_char = alphabet.find(testo_da_cryptare[i])
     new_location_char = location_char + 3
-    testo_cryptato += alfabeto[new_location_char]
+    testo_cryptato += alphabet[new_location_char]
 
 print("Testo cryptato: %s") %testo_cryptato
 
